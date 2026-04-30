43dd7c683dff7da9537d1eb6388b56a8cb84c1c5
Mouss <104678778+MoustaphaCamara@users.noreply.github.com>
2026-04-30 16:09:31 +0200

    chore: packages: linters & readme (#1)

---
+++ frontend/eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import svelte from 'eslint-plugin-svelte';
+import svelteParser from 'svelte-eslint-parser';
+
+export default [
+  {
+    ignores: ['dist/', 'node_modules/', 'wailsjs/', 'build/'],
+  },
+
+  js.configs.recommended,
+
+    // prevent flagging legimitate runtime symbols like Go's 'window.'
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+
+  {
+    files: ['**/*.svelte'],
+    plugins: { svelte },
+    languageOptions: {
+      parser: svelteParser,
+    },
+    rules: {
+      ...svelte.configs.recommended.rules,
+    },
+  },
+];
--- go.mod
+++ go.mod
@@ -4,12 +4,13 @@ go 1.24.0
 
 require (
 	github.com/google/uuid v1.6.0
-	github.com/wailsapp/wails/v2 v2.11.0
+	github.com/wailsapp/wails/v2 v2.12.0
 	golang.org/x/crypto v0.46.0
 	modernc.org/sqlite v1.44.3
 )
 
 require (
+	git.sr.ht/~jackmordaunt/go-toast/v2 v2.0.3 // indirect
 	github.com/bep/debounce v1.2.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
