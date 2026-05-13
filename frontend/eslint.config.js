a14288e409e30bda387cd3c9f3cc2d490ac07198
MoustaphaCamara <cmr.mous@gmail.com>
2026-05-13 10:42:43 +0200

    handle typescript

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,14 +2,15 @@ import js from '@eslint/js';
 import globals from 'globals';
 import svelte from 'eslint-plugin-svelte';
 import svelteParser from 'svelte-eslint-parser';
+import ts from 'typescript-eslint';
 
 export default [
   {
     ignores: ['dist/', 'node_modules/', 'wailsjs/', 'build/'],
   },
-
   js.configs.recommended,
-
+  ...ts.configs.recommended,
+  ...svelte.configs['flat/recommended'],
     // prevent flagging legimitate runtime symbols like Go's 'window.'
   {
     languageOptions: {
@@ -22,13 +23,15 @@ export default [
 
   {
     files: ['**/*.svelte'],
-    plugins: { svelte },
     languageOptions: {
       parser: svelteParser,
-    },
-    rules: {
-      ...svelte.configs.recommended.rules,
-      'no-unused-vars': 'warn',
+      parserOptions: {
+        parser: ts.parser,
+      },
+      // rules: {
+      //   ...svelte.configs.recommended.rules,
+      //   'no-unused-vars': 'warn',
+      // },
     },
   },
 ];
